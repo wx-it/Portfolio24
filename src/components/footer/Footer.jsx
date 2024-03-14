7adfd62bf22c57537096bd618e3fa5f62276ebfe
Lix <zinebbendjafer9@gmail.com>
2024-03-14 16:24:35 +0100

    updated styling for responsivness

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,18 +3,20 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="p-4 md:px-[100px] space-y-8">
-      <div className=" space-y-4 md:flex items-start justify-center md:space-x-16">
-        <h2 className="font-bold text-2xl md:text-5xl md:max-w-[50%]">
+    <footer className="p-4 lg:px-[100px] space-y-8">
+      <div className=" space-y-4 md:flex md:items-center justify-center lg:items-start lg:space-x-16">
+        <h2 className="font-bold text-2xl lg:text-5xl lg:max-w-[50%]">
           Struggling to generate leads and convert website visitors into paying
           customers?
         </h2>
-        <div className="space-y-4">
-          <p className="font-semibold text-base md:text-2xl">
+        <div className="space-y-4 p-0 ">
+          <p className="font-semibold text-base lg:text-2xl">
             Schedule a free consultation today to discuss your landing page
             needs and see how I can help you achieve your goals.
           </p>
-          <p className="font-medium text-xl cursor-pointer">- Book your free consultation</p>
+          <p className="font-medium lg:text-xl cursor-pointer">
+            - Book your free consultation
+          </p>
         </div>
       </div>
       <div className="flex flex-col items-center justify-center space-y-4 md:flex-row md:w-full md:justify-between ">
@@ -29,7 +31,7 @@ const Footer = () => {
             LinkedIn
           </Link>
         </div>
-        <span className="font-medium" >Back to top</span>
+        <span className="font-medium cursor-pointer">Back to top</span>
         <span className="text-sm">Copyright©ZinebBendjafer</span>
       </div>
     </footer>
