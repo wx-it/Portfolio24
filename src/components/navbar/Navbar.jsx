8d4cf7a8a592a9e287e92e387f4ec8168b506ef9
Lix <zinebbendjafer9@gmail.com>
2024-03-14 07:31:51 +0100

    done with navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,21 +7,34 @@ import Link from "next/link";
 const Navbar = () => {
   const [isOpen, setOpen] = useState(false);
   return (
-    <nav className="flex items-center justify-between w-full p-4">
+    <nav className="flex items-center justify-between w-full p-4 md:py-8 md:px-[90px] ">
       <div className="z-10 flex justify-between w-full items-center">
         <Image src="/logo.jpg" width={24} height={44} alt="logo" />
-        <Hamburger
-          toggled={isOpen}
-          toggle={setOpen}
-          onToggle={(toggled) => {
-            if (toggled) {
-              setOpen(true);
-            } else {
-              setOpen(false);
-            }
-          }}
-          hideOutline={false}
-        />
+        <span className="md:hidden">
+          <Hamburger
+            toggled={isOpen}
+            toggle={setOpen}
+            onToggle={(toggled) => {
+              if (toggled) {
+                setOpen(true);
+              } else {
+                setOpen(false);
+              }
+            }}
+            hideOutline={false}
+          />
+        </span>
+        <div className=" hidden items-center justify-center space-x-8 md:flex">
+          <Link className=" text-lg" href={"/"}>
+            Projects
+          </Link>
+          <Link className=" text-lg" href={"/"}>
+            Services
+          </Link>
+          <Link className=" text-lg" href={"/"}>
+            Contact
+          </Link>
+        </div>
       </div>
       <div
         className={
