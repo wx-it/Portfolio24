622a5caf5088ba7d546ca2b147c962b6730360a5
Lix <zinebbendjafer9@gmail.com>
2024-03-26 13:16:37 +0100

    added icons + some tweaks

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,9 +14,28 @@ const Footer = () => {
             Schedule a free consultation today to discuss your landing page
             needs and see how I can help you achieve your goals.
           </p>
-          <p className="font-medium lg:text-xl cursor-pointer">
-            - Book your free consultation
-          </p>
+          <div className="flex items-center justify-start space-x-1 group">
+            <p className="font-medium lg:text-xl cursor-pointer">
+              - Book your free consultation
+            </p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1.25"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="group-hover:translate-x-1 transition-all ease-in duration-[.5s]"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M5 12l14 0" />
+              <path d="M15 16l4 -4" />
+              <path d="M15 8l4 4" />
+            </svg>
+          </div>
         </div>
       </div>
       <div className="flex flex-col items-center justify-center space-y-4 md:flex-row md:w-full md:justify-between ">
@@ -31,7 +50,27 @@ const Footer = () => {
             LinkedIn
           </Link>
         </div>
-        <span className="font-medium cursor-pointer">Back to top</span>
+        <div className="flex items-center justify-center space-x-1">
+          <span className="font-medium cursor-pointer">Back to top</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="icon icon-tabler icons-tabler-outline icon-tabler-arrow-up"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M12 5l0 12" />
+            <path d="M18 11l-6 -6" />
+            <path d="M6 11l6 -6" />
+          </svg>
+        </div>
+
         <span className="text-sm">Copyright©ZinebBendjafer</span>
       </div>
     </footer>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,16 +4,16 @@ const Hero = () => {
   return (
     <div className="p-4 md:px-12 lg:px-[90px] md:h-screen">
       <span className="text-xl">{`I'm a`}</span>
-      <h1 className="font-bold text-5xl md:text-7xl lg:text-9xl ">
+      <h1 className="font-bold text-5xl md:text-7xl lg:text-[150px] w-full max-w-[80%] ">
         Frontend Web Developer
       </h1>
-      <div className="flex items-center justify-start space-x-1 mt-2">
+      <div className="flex items-center justify-start space-x-1 md:space-x-3 mt-2 lg:mt-5">
         <span className="bg-[#1D1D1D] w-6 h-6 rounded-full lg:w-8 lg:h-8"></span>
-        <p className="text-xl font-medium lg:text-3xl">
+        <p className="text-xl font-medium lg:text-3xl md:tracking-wider">
           I specialize in optimizing generic
         </p>
       </div>
-      <p className="text-xl font-medium lg:text-3xl">
+      <p className="text-xl font-medium lg:text-3xl md:tracking-wider">
         landing pages for maximum impact.
       </p>
       <button className="group relative overflow-hidden  border border-black rounded-full px-4 py-3 font-medium text-base mt-4 lg:text-2xl lg:py-4 lg:px-6">
